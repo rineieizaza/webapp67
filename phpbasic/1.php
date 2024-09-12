f075ca1fc60a0944121c789a583b8809afbfeb89
rineieizaza <narakteesub123456@gmail.com>
2024-09-12 15:34:52 +0700

    Add files via upload

---
+++ phpbasic/1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello php</title>
+</head>
+<body>
+    <?php
+       echo "Hello PHP";
+       echo "<br>";
+       echo "this is my first time writw PHP Langual";
+    echo "<br>";
+    echo "<br>";
+           define("MINSIZE",50);
+       echo MINSIZE;
+    ?>
+</body>
+</html>
